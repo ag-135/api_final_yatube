6a42478ea008bbe82125c36f1f6b7916fee76a9e
Андрей Грицков <gricai39@yandex.ru>
2022-08-31 18:52:52 +0300

    First_review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,17 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import include, path
+from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowsViewSet
+from rest_framework.authtoken import views
+
+v1_router = routers.DefaultRouter()
+v1_router.register(r'posts', PostViewSet)
+v1_router.register(r'groups', GroupViewSet)
+v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comment')
+v1_router.register(r'follow', FollowsViewSet)
 
 urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
